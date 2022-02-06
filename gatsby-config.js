c68d5231b125b949c70989e6b447d5530a1da4c6
Noe Aguilar Eseiza <noe@iqkitche.co>
2022-02-05 19:52:25 -0600

    changed framework to gatsby and added pre commit hook to prettier

---
+++ gatsby-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteMetadata: {
+    title: `Issis' Research Group`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-160667109-1",
+      },
+    },
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import "../styles/global.css";
+
+// markup
+const IndexPage = () => {
+  const linkStyle = `
+    Hover:opacity-70
+    text-sm
+    sm:text-lg
+   `;
+  const logoStyle = `
+   text-white 
+   font-rammetto
+   sm:text-base
+   text-sm
+   `;
+  return (
+    <div>
+      <nav className="flex sm:justify-between justify-around items-center bg-green-900 lg:px-20 sm:px-6 py-8 text-gray-100">
+        <h3 className={logoStyle}>HealthRoom</h3>
+        <ul className="flex">
+          <li className={linkStyle}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={linkStyle}>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li className={linkStyle}>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <main>
+        <span>...</span>
+      </main>
+
+      <footer className="text-center py-8 bg-green-900 text-gray-100">
+        <p>
+          Copyright 2030 <span className={logoStyle}>HealthRoom</span>
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default IndexPage;
