601e59f1477cc7f597fa45d3efe5acd3fdba7cc3
Claudio <claudio.tapia@gmx.com>
2022-03-01 22:16:30 -0300

    paso

--- src/components/Calcs/cuentas.js
+++ src/components/Calcs/cuentas.js
@@ -2,18 +2,7 @@
 // import ParameterFormular from '../ParameterFormular/ParameterFormular'
 import { form_feed } from './test'
 import { setSuma } from '../Resultados/slice'
-import { CU_THICK } from '../../utils/constants'
-
-//valores fijos
-const fixed = {
-  al_density: 2.7,
-  cu_density: 8.96,
-  electrolite_density: 1.5,
-  separator_density: 0.97,
-  cathode_porosity: 0.3,
-  anode_porosity: 0.3,
-  separator_porosity: 0.55,
-}
+import * as fixed from '../../utils/constants'
 
 /* estas variables deben ser respuestas de consultas a la api con la info ingresada
 const s_rate_anode_capacity=anode_material_id AND anode_real_capacity;
@@ -25,10 +14,8 @@ const current_max=(cathode_material_id AND cathode_theor_charge)*fast_charge_rat
 const f_rate_cathode_capacity=cathode_material_id AND fast_charge_rate_id AND cathode_capacity
 const f_rate_charge_voltage=cathode_material_id AND fast_charge_rate_id AND cathode_charge_voltage
 const f_rate_discharge_voltage=(cathode_material_id AND fast_charge_rate_id AND cathode_discharge_voltage)-(anode_material_id AND anode_voltage);
-*/
 
-//solo para pruebitas
-
+Respuestas de api, ver nombres de variables (prefijo PR_)
 const s_rate_anode_capacity = 175
 const s_rate_cathode_capacity = 145
 const s_rate_charge_voltage = 2.35
@@ -38,11 +25,11 @@ const current_max = 0.7
 const f_rate_cathode_capacity = 130
 const f_rate_charge_voltage = 4
 const f_rate_discharge_voltage = 2.2
-
-//solo para pruebitas
+*/
 
 //calculos preliminares
 export function calcExam(data, dispatch) {
+  console.log(data)
   const {
     cathode_material_id,
     anode_material_id,
@@ -61,85 +48,102 @@ export function calcExam(data, dispatch) {
     n_series,
     n_parallel,
   } = { ...data }
+
+  const charge_thickness_dependency_cda = 50 * cathode_load
+  const cathode_mass = area * cathode_load * n_coat * 0.001
+  const cathode_additives = (cathode_mass * cathode_add) / (100 - cathode_add)
+  const cathode_mass_st = cathode_mass + cathode_add
+  const cathode_collector_mat =
+    area * curr_collect_thickness_al * fixed.AL_DENSITY * 0.0001
+  const cathode_total_mass = cathode_mass_st + cathode_collector_mat
+  const anode_mass =
+    (fixed.PR_s_rate_cathode_capacity / fixed.PR_s_rate_anode_capacity) *
+    area *
+    cathode_load *
+    n_coat *
+    0.001
+  const anode_additives = (anode_mass * anode_add) / (100 - anode_add)
+  const anode_mass_st = anode_mass + anode_additives
+  const anode_collector_mat =
+    area * curr_collect_thickness_cu * fixed.CU_DENSITY * 0.0001
+  const anode_total_mass = anode_mass_st + anode_collector_mat
+  const electrodes_total_mass = cathode_total_mass + anode_total_mass
+  const separator_mass =
+    area * separator_thickness * fixed.SEPARATOR_DENSITY * 0.0001
+  //base_unit
+  const electrolite_cathode_mass =
+    area *
+    charge_thickness_dependency_cda *
+    fixed.CATHODE_POROSITY *
+    n_coat *
+    0.0001
+  const electrolite_anode_mass =
+    area *
+    charge_thickness_dependency_cda *
+    fixed.ANODE_POROSITY *
+    n_coat *
+    0.0001
+  const electrolite_separator_mass =
+    area *
+    separator_thickness *
+    fixed.ELECTROLITE_DENSITY *
+    fixed.SEPARATOR_POROSITY *
+    0.0001
+  const electrolite_total_mass =
+    electrolite_cathode_mass +
+    electrolite_anode_mass +
+    electrolite_separator_mass
+  const base_unit_total_mass =
+    electrodes_total_mass + separator_mass + electrolite_total_mass
+  //cell
+  const cell_electrodes_mass = electrodes_total_mass * n_base_units
+  const cell_electrolite_in_electrodes_mass =
+    (electrolite_cathode_mass + electrolite_anode_mass) * n_base_units
+  const cell_separator_and_electrolite_mass =
+    (separator_mass + electrolite_separator_mass) * (2 * n_base_units - 1) //ver formula
+  const cell_total_mass =
+    cell_electrodes_mass +
+    cell_electrolite_in_electrodes_mass +
+    cell_separator_and_electrolite_mass
+  //modeule
+  const module_total_mass = cell_total_mass * n_series * n_parallel * 0.001
+
   const before_calc = {
-    charge_thickness_dependency_cda: 50 * cathode_load,
-    // cathode_mass:
-    //   form_feed.area.value *
-    //   form_feed.cathode_load.value *
-    //   form_feed.n_coat.value *
-    //   0.001,
-    // cathode_additives:
-    //   (cathode_mass * form_feed.cathode_add.value) /
-    //   (100 - form_feed.cathode_add.value),
-    // cathode_mass_st: cathode_mass + form_feed.cathode_add.value,
-    // cathode_collector_mat:
-    //   form_feed.area.value *
-    //   form_feed.curr_collect_thickness_al.value *
-    //   fixed.al_density *
-    //   0.0001,
-    // cathode_total_mass: cathode_mass_st + cathode_collector_mat,
-    // anode_mass:
-    //   (s_rate_cathode_capacity / s_rate_anode_capacity) *
-    //   area *
-    //   cathode_load *
-    //   n_coat *
-    //   0.001,
+    charge_thickness_dependency_cda,
+    cathode_mass,
+    cathode_additives,
+    cathode_mass_st,
+    cathode_collector_mat,
+    cathode_total_mass,
+    anode_mass,
+    anode_additives,
+    anode_mass_st,
+    anode_collector_mat,
+    anode_total_mass,
+    electrodes_total_mass,
+    separator_mass,
+    electrolite_cathode_mass,
+    electrolite_anode_mass,
+    electrolite_separator_mass,
+    electrolite_total_mass,
+    base_unit_total_mass,
+    cell_electrodes_mass,
+    cell_electrolite_in_electrodes_mass,
+    cell_separator_and_electrolite_mass,
+    cell_total_mass,
+    module_total_mass,
   }
 
-  const sumotaRieman = area + 3.141516 + CU_THICK
+  /*
+  const sumotaRieman = area + n_series //CU_THICK
 
   dispatch(setSuma(sumotaRieman))
+  */
+  console.log(before_calc)
+  dispatch(setSuma(before_calc))
+
   //console.log(before_calc.charge_thickness_dependency_cda)
 }
-
-/*
-Procesadas
-**********
-const charge_thickness_dependency_cda = 50 * cathode_load
-const cathode_mass = area * cathode_load * n_coat * 0.001
-const cathode_additives = (cathode_mass * cathode_add) / (100 - cathode_add)
-const cathode_mass_st = cathode_mass + cathode_additives
-const cathode_collector_mat = area * curr_collect_thickness_al * al_density * 0.0001
-const cathode_total_mass = cathode_mass_st + cathode_collector_mat
-
-********** 
-*/
-/*
-const anode_additives = (anode_mass * anode_add) / (100 - anode_add)
-const anode_mass_st = anode_mass + anode_additives
-const anode_collector_mat =
-  area * curr_collect_thickness_cu * cu_density * 0.0001
-const anode_total_mass = anode_mass_st + anode_collector_mat
-
-const electrodes_total_mass = cathode_total_mass + anode_total_mass
-
-const separator_mass = area * separator_thickness * separator_density * 0.0001
-
-//base unit
-const electrolite_cathode_mass =
-  area * charge_thickness_dependency_cda * cathode_porosity * n_coat * 0.0001
-const electrolite_anode_mass =
-  area * charge_thickness_dependency_cda * anode_porosity * n_coat * 0.0001
-const electrolite_separator_mass =
-  area * separator_thickness * electrolite_density * separator_porosity * 0.0001
-const electrolite_total_mass =
-  electrolite_cathode_mass + electrolite_anode_mass + electrolite_separator_mass
-const base_unit_total_mass =
-  electrodes_total_mass + separator_mass + electrolite_total_mass
-
-//cell
-const cell_electrodes_mass = electrodes_total_mass * n_base_units
-const cell_electrolite_in_electrodes_mass =
-  (electrolite_cathode_mass + electrolite_anode_mass) * n_base_units
-const cell_separator_and_electrolite_mass =
-  (separator_mass + electrolite_separator_mass) * (2 * n_base_units - 1) //ver formula
-const cell_total_mass =
-  cell_electrodes_mass +
-  cell_electrolite_in_electrodes_mass +
-  cell_separator_and_electrolite_mass
-
-//module
-const module_total_mass = cell_total_mass * n_series * n_parallel * 0.001
 
 //BAJAS CORRIENTES
 /*
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,19 @@
 export const CU_THICK = 5.123
+export const AL_DENSITY = 2.7
+export const CU_DENSITY = 8.96
+export const ELECTROLITE_DENSITY = 1.5
+export const SEPARATOR_DENSITY = 0.97
+export const CATHODE_POROSITY = 0.3
+export const ANODE_POROSITY = 0.3
+export const SEPARATOR_POROSITY = 0.55
+
+//provisorias hasta api
+export const PR_s_rate_anode_capacity = 175
+export const PR_s_rate_cathode_capacity = 145
+export const PR_s_rate_charge_voltage = 2.35
+export const PR_s_rate_discharge_voltage = 2.3
+export const PR_current_min = 0.014
+export const PR_current_max = 0.7
+export const PR_f_rate_cathode_capacity = 130
+export const PR_f_rate_charge_voltage = 4
+export const PR_f_rate_discharge_voltage = 2.2
