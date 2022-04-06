ee0cff5d35bd6931501d6a5893628858728a8e42
Noe Aguilar Eseiza <noe@iqkitche.co>
2022-04-06 15:25:09 -0500

    :..

--- src/components/ManageMats/ManageMatsForm.jsx
+++ src/components/ManageMats/ManageMatsForm.jsx
@@ -1,9 +1,9 @@
 import React, { useReducer, useState, useEffect } from 'react'
 import '../../styles/global.css'
 import { useForm } from 'react-hook-form'
-import {  useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import useSWR from 'swr'
-import { getMaterialsFetcher,propsCall } from '../../api'
+import { getMaterialsFetcher, propsCall } from '../../api'
 
 export default function ModMatsForm() {
   const dispatch = useDispatch()
@@ -25,18 +25,19 @@ export default function ModMatsForm() {
     '?filter=%7B%22order%22%3A%20%22type%22%2C%0A%20%22order%22%3A%20%22name%22%2C%0A%20%20%22fields%22%3A%20%7B%0A%20%20%20%20%22name%22%3A%20true%2C%0A%20%20%20%20%22type%22%3A%20true%2C%0A%20%20%20%20%22c_rates%22%3A%20true%2C%0A%20%20%20%20%22id%22%3A%20true%0A%20%20%7D%0A%7D',
     getMaterialsFetcher
   )
-  const watchedMat = watchedMat === undefined ? {} : watch('selectedMat')
-  
-  
+  const watchedMat = watch('selectedMat')
+  const [fetchedMaterial, setFetchedMat] = useState({})
 
+  useEffect(async () => {
+    const myMaterial = await propsCall(watchedMat)
+    setFetchedMat(myMaterial)
+    console.log({ myMaterial })
+  }, [watchedMat])
 
   return (
     <form
       onSubmit={handleSubmit(async (manma_data) => {
-        
-      const respMat = respMat === undefined ? {} :await propsCall(watchedMat)
-      
-
+        const respMat = respMat === undefined ? {} : await propsCall(watchedMat)
       })}
       className=" shadow sm:rounded-md bg-gray-100 "
     >
@@ -65,12 +66,12 @@ export default function ModMatsForm() {
 
 */}
 
-{console.log(watchedMat)}
+      {console.log(watchedMat)}
       <div className="flex items-baseline mt-2 mb-2 pb-1 border-slate-200"></div>
 
       <h2 class="text-xl font-extrabold tracking-tight text-gray-900 mx-4 pt-3">
         Modify or Delete a Material
-      </h2> 
+      </h2>
       <div className="flex items-baseline mt-2 mb-2 pb-1 border-slate-200"></div>
       <div className=" flex flex-row gap-2 flex items-center ">
         <div className="basis-1/2 md:basis-1/5">
@@ -80,47 +81,53 @@ export default function ModMatsForm() {
         </div>
 
         <div className="basis-1/2 md:basis-1/3 -mx-8">
-        <select
+          <select
             {...register('selectedMat')}
             type="text"
-            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-          {!data ? (
-            <option value="-1">Loading</option>
-          ) : (
-            <>
-              <option value="" selected> Select one </option>
-              {data.map((elem) => (
-                <option value={elem.id}>{elem.name}</option>
-              ))}
-            </>
-          )}
-          </select> 
-        </div>
-        </div>
- 
-        <div className="flex items-baseline mt-2 mb-2 pb-1 border-slate-200"></div>
-
-        <label className="block text-l font-medium text-gray-700 mx-4 ">
-            Material properties
-          </label>
-
-          <div>
-
-          </div>
-
-    
-
-        <div className="col-span-1">
-          <label className="block text-sm font-medium text-gray-700">
-            anotherfield
-          </label>
-          <input
-            {...register('anotherfield')}
-            type="text"
             className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-          />
+          >
+            {!data ? (
+              <option value="-1">Loading</option>
+            ) : (
+              <>
+                <option value="" selected>
+                  {' '}
+                  Select one{' '}
+                </option>
+                {data.map((elem) => (
+                  <option value={elem.id}>{elem.name}</option>
+                ))}
+              </>
+            )}
+          </select>
         </div>
-     
+      </div>
+
+      <div className="flex items-baseline mt-2 mb-2 pb-1 border-slate-200"></div>
+
+      <label className="block text-l font-medium text-gray-700 mx-4 ">
+        Material properties
+      </label>
+
+      <div>
+        {Object.keys(fetchedMaterial?.properties || {}).map((elem) => {
+          const prop = fetchedMaterial.properties[elem]
+          if (typeof prop !== 'undefined') {
+            return <input value={prop.value} />
+          }
+        })}
+      </div>
+
+      <div className="col-span-1">
+        <label className="block text-sm font-medium text-gray-700">
+          anotherfield
+        </label>
+        <input
+          {...register('anotherfield')}
+          type="text"
+          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+        />
+      </div>
 
       <div className="px-4 py-3 bg-gray-50 text-right sm:px-6 mt-2">
         <button
